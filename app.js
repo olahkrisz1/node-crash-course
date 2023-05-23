f13da2481e11f05e375de00a2e92fdf8d0323c47
olahkrisz1 <olahkrisz1@gmail.com>
2023-05-23 19:59:32 +0200

    ejs view engine

--- app.js
+++ app.js
@@ -2,27 +2,46 @@ const express = require("express");
 
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 
 app.listen(3000);
 
+// app.get("/", (req, res) => {
+//   //res.send("<p>home page</p>");
+//   res.sendFile("./views/index.html", { root: __dirname });
+// });
+
 app.get("/", (req, res) => {
-  //res.send("<p>home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
+// app.get("/about", (req, res) => {
+//   res.send("<p>about page</p>");
+// });
+
 app.get("/about", (req, res) => {
-  res.send("<p>about page</p>");
+  res.render("about");
 });
 
 // redirect
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+// app.get("/about-us", (req, res) => {
+//   res.redirect("/about");
+// });
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404 page
 
+// app.use((req, res) => {
+//   res.status(404).sendFile("./views/404.html", { root: __dirname });
+// });
+
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
